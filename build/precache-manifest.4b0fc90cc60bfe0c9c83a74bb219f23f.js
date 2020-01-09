cc99f5d0ccec88a5a6799fe03d02e5ef382488a0
Diana Felix <dianasf.f@gmail.com>
2020-01-09 20:51:10 -0300

    build

---
+++ build/precache-manifest.4b0fc90cc60bfe0c9c83a74bb219f23f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14ce2b253100eb945d2ab64c6e5b584",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0b2ba673106efcb194d",
+    "url": "/static/css/main.19ce15f1.chunk.css"
+  },
+  {
+    "revision": "0010431124dbb813d936",
+    "url": "/static/js/2.07922a74.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.07922a74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0b2ba673106efcb194d",
+    "url": "/static/js/main.a29a7e52.chunk.js"
+  },
+  {
+    "revision": "c1cfc31b20567a8ec094",
+    "url": "/static/js/runtime-main.aeb1d266.js"
+  },
+  {
+    "revision": "eccc6f8a0d254bba439789ec31657700",
+    "url": "/static/media/brandi-redd-unsplash.eccc6f8a.jpg"
+  }
+]);
